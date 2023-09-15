0deb569da410cbe027fab0b1ca46302e3011e37b
Ming Herng <mingherng@apppay.tech>
2023-09-15 21:05:30 +0800

    Login Page Version 1

---
+++ login.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+// this con is used to connect with the database
+$con=mysqli_connect("localhost", "root", null, "cocomelon");
+//declaration of errors for error validation
+$errors = array();
+if (isset($_POST['login'])) {
+        $user_username = mysqli_real_escape_string($con, $_POST['sname']);
+        $user_password = mysqli_real_escape_string($con, $_POST['spsw']);
+
+
+        if (count($errors) == 0) {
+		// this query is used to track student id and password
+                $query = "SELECT * FROM Login WHERE Username ='$user_username' AND Password ='$user_password'";
+
+                $result = mysqli_query($con, $query);
+                $row = mysqli_fetch_assoc($result);
+				
+                if (mysqli_num_rows($result) == 1) {
+                        
+						//header('location: ILBServer.php');
+                }else {
+                        array_push($errors, "Wrong username/password combination");
+                }
+                mysqli_free_result($result);
+                mysqli_close($con);                        
+        }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>SmashIt Badminton Login</title>
+<style>
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Control the left side */
+.left {
+  left: 0;
+  background-color: blue;
+}
+
+/* Control the right side */
+.right {
+  right: 0;
+  background-color: orange;
+}
+
+/* If you want the content centered horizontally and vertically */
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: left;
+}
+
+
+---------------------------------------------------------------
+body {font-family: Arial, Helvetica, sans-serif;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.container {
+    border-radius: 35px;
+    background-color: white;
+    background-position: left top;
+    background-repeat: repeat;
+    padding: 50px;
+    width: 70%;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
+</head>
+<body>
+<div class="split left">
+  <div class="centered">
+  <img src="BmtLogo.png" alt="SmashItBadminton" width="400px" height="350px">
+  </div>
+</div>
+
+<div class="split right">
+  <div class="centered">
+    <form action="/action_page.php" method="post">
+        <div class="container">
+            <img src="" alt="LoginIcon" width="400px" height="350px">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+            
+            <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+            <button type="submit">Login</button>
+        </div>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+
+    </form>
+  </div>
+</div>
+
+</body>
+</html>
