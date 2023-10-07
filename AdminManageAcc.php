dd8a4a8d632e1be12760a9b724988357e5ec53fe
Joeyee1223 <91905004+Joeyee1223@users.noreply.github.com>
2023-10-07 18:27:46 +0800

    Update AdminManageAcc.php

--- AdminManageAcc.php
+++ AdminManageAcc.php
@@ -23,7 +23,7 @@ session_start();
                 <a href="addbooking.php">Add Booking</a>
                 <a href="editbooking.php">Check Booking</a>
                 <a href="membership.php">Membership Management</a>
-                <a href="edittimetable.php">Trainer Timetable</a>
+                <a href="adminedittimetablestatus.php">Trainer Timetable</a>
                 <a href="adminmanageacc.php">Manage Account</a>
             </div>
         </div>
