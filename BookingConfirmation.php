3e6d8f21fae4c21ebd03a56fdf6a9cec5376e51b
Joeyee1223 <91905004+Joeyee1223@users.noreply.github.com>
2023-10-07 20:11:08 +0800

    Update BookingConfirmation.php

--- BookingConfirmation.php
+++ BookingConfirmation.php
@@ -116,6 +116,7 @@ if (isset($_POST['book_appointment'])) {
                     echo "<div class='dropdown-content'>";
                     echo "<a href='customertimetable.php'>Trainer Timetable</a>";
                     echo "<a href='addbooking.php'>Book Court Now!</a>";
+                    echo "<a href='editbooking.php'>Any Changes To Bookings</a>";
                     echo "</div></div>";
                 }else if($row['Roles'] == 'trainer'){
                     echo "<a href='trainerhome.php#home'><b>Home</b></a>";
