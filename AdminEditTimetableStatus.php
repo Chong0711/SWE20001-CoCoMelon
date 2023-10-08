18d9bf271fdb9b355ce0ebc95958f27da5e30a0b
BlaTeh <143069305+BlaTeh@users.noreply.github.com>
2023-10-08 11:59:40 +0800

    Update AdminEditTimetableStatus.php

--- AdminEditTimetableStatus.php
+++ AdminEditTimetableStatus.php
@@ -183,7 +183,6 @@ body{
    display: none;
    position: absolute;
    background-color: transparent; /* Set dropdown background to transparent */
-   min-width: 160px;
    z-index: 1;
    top: 100%;
    left: 0; 
@@ -192,8 +191,10 @@ body{
 
 /* Links inside the dropdown */
 .dropdown-content a {
+   font-size: 16px;
    color: #44561c;
-   padding: 12px 16px;
+   padding: 12px 14px;
+   width: 117px;
    text-decoration: none;
    display: block;
 }
