eb870dc599a7905e7bda709e4bc9880c92efb808
BlaTeh <143069305+BlaTeh@users.noreply.github.com>
2023-10-27 20:05:57 +0800

    Update EditBooking.php

--- EditBooking.php
+++ EditBooking.php
@@ -273,8 +273,8 @@ body{
 /*Dropdown Menu*/
 /* Dropdown container */
 
-.navigation a:nth-child(3) {
-   margin-right: 40px; /* Adjust the margin value as needed */
+.navigation a:nth-child(4) {
+   margin-right: 30px; /* Adjust the margin value as needed */
 }
 
 .dropdown {
