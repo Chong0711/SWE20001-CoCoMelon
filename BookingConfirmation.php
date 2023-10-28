aba235b462a7ea4d570aa241dfde71acbb637ce2
Joeyee1223 <91905004+Joeyee1223@users.noreply.github.com>
2023-10-28 13:59:13 +0800

    added faq

--- BookingConfirmation.php
+++ BookingConfirmation.php
@@ -110,6 +110,7 @@ if (isset($_POST['book_appointment'])) {
             echo "<div class='dropdown'>";
             echo "<button class='dropbtn'><b>Services</b></button>";
             echo "<div class='dropdown-content'>";
+            echo "<a href='faq.php'>FAQ</a>";
             echo "<a href='addbooking.php'>Book Court Now!</a>";
             echo "</div></div>";
             echo "<a href='login.php'><b>Login</b></a>";
@@ -127,6 +128,7 @@ if (isset($_POST['book_appointment'])) {
                         echo "<div class='dropdown'>";
                         echo "<button class='dropbtn'><b>Services</b></button>";
                         echo "<div class='dropdown-content'>";
+                        echo "<a href='faq.php'>FAQ</a>";
                         echo "<a href='customertimetable.php'>Trainer Timetable</a>";
                         echo "<a href='addbooking.php'>Book Court Now!</a>";
                         echo "<a href='editbooking.php'>Any Changes To Bookings</a>";
